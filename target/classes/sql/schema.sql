aaec7cdef82252425e5b5eb34c82fbbc9f94d0a8
Rehman Arshad <rehmanarshad@macbook-pro.home>
2021-04-16 00:59:29 +0100

    adding gitignore

--- target/classes/sql/schema.sql
+++
@@ -1,9 +0,0 @@
---create database demotask;
-\connect demotask;
-
-CREATE TABLE MyTasks(
-taskID UUID PRIMARY KEY,
-owner VARCHAR (50) NOT NULL,
-description VARCHAR(50) NOT NULL
-);
-INSERT INTO MyTasks (taskID,owner,description) VALUES ('61614667-d279-11e7-a5ac-f941ac8dfc39','rehman','spring lecture');
--- target/test-classes/data.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists MyTasks;
-CREATE TABLE MyTasks (
- taskID UUID PRIMARY KEY,
- owner VARCHAR (50) NOT NULL,
- description VARCHAR (50) NOT NULL
-);
-INSERT INTO MyTasks (taskID,owner,description) values ('61614667-d279-11e7-a5ac-f941ac8dfc39','ray','yo');
